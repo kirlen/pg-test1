0d2981140a9b57a6ed0fdec01bddee55ca74db4b
Kevin Irlen <kevin.irlen@gmail.com>
2017-09-19 11:06:35 -0400

    use connection string and ssl true

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const port = process.env.PORT;
 const restify = require('restify');
 const { Client } = require('pg');
 
+// const client = new Client({
+//     host     : process.env.PG_HOST,
+//     port     : process.env.PG_PORT,
+//     user     : process.env.PG_USER,
+//     password : process.env.PG_PASSWORD,
+//     database : process.env.PG_DATABASE
+// });
 const client = new Client({
-    host     : process.env.PG_HOST,
-    port     : process.env.PG_PORT,
-    user     : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    database : process.env.PG_DATABASE
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
 });
 
 client.connect();
